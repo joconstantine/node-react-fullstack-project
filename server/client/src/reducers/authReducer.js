283cc3d50660b73bf87f3428536e5debe1645da7
joconstantine <joconstantine@users.noreply.github.com>
2021-08-22 18:07:58 +0800

    22/08/2021 18:07

--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,10 +1,12 @@
 import { FETCH_USER } from '../actions/types';
 
-export default function (state = null, action) {
+const authReducer = function (state = null, action) {
   switch (action.type) {
     case FETCH_USER:
       return action.payload || false; // return false if payload is empty
     default:
       return state;
   }
-}
+};
+
+export default authReducer;
